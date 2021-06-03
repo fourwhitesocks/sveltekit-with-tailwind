2f8fff66375cf86e7c80b12c32b0ed850d0c5e10
amy stout <asbilly92@gmail.com>
2021-06-03 15:02:16 -0400

    added some more tw

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,9 +19,11 @@
 			</picture>
 		</div>
 
-		to your new<br />SvelteKit app
+		Some TAILWIND<br />here! Too cool!
 	</h1>
-	<p>This is a copy of the regular SvelteKit Demo I did already but it has tailwinds in it</p>
+	<p class="text-green-600 mt-12">
+		This is a copy of the regular SvelteKit Demo I did already but it has tailwinds in it
+	</p>
 	<p>From this video: https://youtu.be/_bViHP-Utxw</p>
 	<p>I got help with this from here: https://github.com/svelte-add/tailwindcss/issues/42</p>
 	<p />
@@ -30,6 +32,51 @@
 	<h2>
 		try editing <strong>src/routes/index.svelte</strong>
 	</h2>
+
+	<ul class="list-disc space-y-2 mt-10">
+		<li class="flex items-start">
+			<span class="h-6 flex items-center sm:h-7">
+				<svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</span>
+			<p class="ml-2">
+				Customizing your
+				<code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+			</p>
+		</li>
+		<li class="flex items-start">
+			<span class="h-6 flex items-center sm:h-7">
+				<svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</span>
+			<p class="ml-2">
+				Push to Netlify!
+				<code class="text-sm font-bold text-gray-900">@apply</code>
+			</p>
+		</li>
+		<li class="flex items-start">
+			<span class="h-6 flex items-center sm:h-7">
+				<svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</span>
+			<p class="ml-2">YAY!!</p>
+		</li>
+	</ul>
 
 	<Counter />
 </section>
