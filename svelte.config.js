3f82c9b237b8b01deaf2d75f44e6ef3ff06286e5
amy stout <asbilly92@gmail.com>
2021-06-03 14:31:49 -0400

    fix svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
-import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
-
 import adapter from '@sveltejs/adapter-netlify'
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter()
-
-
 	}
 };
 
